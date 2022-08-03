cb31962b828b7716a6037db79b252ebccc30676b
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-08-03 15:49:32 +0600

    patient list page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:doc_hub/pages/add_patient_page.dart';
 import 'package:doc_hub/pages/home_page.dart';
 import 'package:doc_hub/pages/login_page.dart';
+import 'package:doc_hub/pages/patient_list_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,14 +15,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
 
         primarySwatch: Colors.blue,
       ),
-      home: const AddPatientPage()
+      home: const PatientListPage()
     );
   }
 }
--- lib/pages/patient_list_page.dart
+++ lib/pages/patient_list_page.dart
@@ -1,3 +1,5 @@
+import 'package:doc_hub/utils/theme.dart';
+import 'package:doc_hub/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
 class PatientListPage extends StatelessWidget {
@@ -6,7 +8,110 @@ class PatientListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          "Patients",
+          style: titleStyle.copyWith(fontSize: 20),
+        ),
+        leading: GestureDetector(
+          onTap: () {},
+          child: Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(onPressed: (){
+            showSearch(
+                context: context,
+                delegate: MySearchDelegate());
+          },
+            icon: Icon(Icons.search, color: Colors.black,),
+          )
+        ],
+      ),
+      body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(top: 10,),
+                      padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Color(0xFFe8e8e8),
+                                blurRadius: 5.0,
+                                offset: Offset(0, 5)),
+                            BoxShadow(color: Colors.white, offset: Offset(-5, 0)),
+                          ]
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomText(text: "Fahim", textColor: Colors.black, fontSize: 20,
+                            weight: FontWeight.bold,),
+                          SizedBox(height: 5,),
+                          Row(
+                            children: [
+                              Text("Patient Age: 23",
+                                style: subTitleStyle,
+                              ),
+                              SizedBox(width: 10,),
+                              Text("Patient Weight: 87",
+                                style: subTitleStyle,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 5,),
+                          Text("Diseases: Cancer",
+                            style: titleStyle,
+                          )
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+          ],
+      ),
     );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate{
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+      icon: Icon(Icons.clear),
+      onPressed: (){
+
+      },
+    )];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
   }
 }
